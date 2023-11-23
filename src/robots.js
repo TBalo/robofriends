0a82d078bf127d3b5c1e61bc008e04987655a324
TBalo <tbalog426@gmail.com>
2023-11-23 17:25:10 +0100

    add robofriends

---
+++ src/robots.js
@@ -0,0 +1,57 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Tomi Balogun',
+        username: 'Tomb',
+        email: 'Land@gmail.com'
+    },
+
+    {
+        id: 2,
+        name: 'Kemi Balogun',
+        username: 'Kemade',
+        email: 'Kk@gmail.com'
+    },
+
+    {
+        id: 3,
+        name: 'Tolu Balogun',
+        username: 'Tbalo',
+        email: 'ToBa@gmail.com'
+    },
+
+    {
+        id: 4,
+        name: 'Bolu Balogun',
+        username: 'Boba',
+        email: 'Bolu@gmail.com'
+    },
+
+    {
+        id: 5,
+        name: 'Jomi Oke',
+        username: 'Joke',
+        email: 'Jom@gmail.com'
+    },
+
+    {
+        id: 6,
+        name: 'Ayo Balogun',
+        username: 'Ay',
+        email: 'Rinde@gmail.com'
+    },
+
+    {
+        id: 7,
+        name: 'Olabisi Balogun',
+        username: 'Florence',
+        email: 'Oladimeji@gmail.com'
+    },
+
+    {
+        id: 8,
+        name: 'Sunday Balogun',
+        username: 'Babatunde',
+        email: 'Tunde@gmail.com'
+    },
+]
